c98c87aa90b3a796505c6888029a4973584748a7
anhcraft <admin@anhcraft.dev>
2019-10-09 20:28:33 +0700

    Updated ConfigDoc

--- src/main/java/dev/anhcraft/confighelper/ConfigDoc.java
+++ src/main/java/dev/anhcraft/confighelper/ConfigDoc.java
@@ -16,6 +16,7 @@ import java.util.List;
 
 public class ConfigDoc {
     private final List<ConfigSchema<?>> schemas = new ArrayList<>();
+    private String footer = "<footer>Powered by ConfigDoc/<a href=\"https://github.com/anhcraft/confighelper/\">ConfigHelper</a></footer>";
 
     @Contract("_ -> this")
     public ConfigDoc withSchema(@NotNull ConfigSchema<?> schema){
@@ -32,18 +33,34 @@ public class ConfigDoc {
     }
 
     @Contract("_ -> this")
+    public ConfigDoc footer(@NotNull String footer){
+        Preconditions.checkNotNull(footer);
+        this.footer = footer;
+        return this;
+    }
+
+    @Contract("_ -> this")
     public ConfigDoc generate(@NotNull File output){
         Preconditions.checkNotNull(output);
         output.mkdirs();
 
-        StringBuilder ovBuilder = new StringBuilder("<!Doctype html><html><head><title>Overview</title><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><meta name=\"robots\" content=\"none\"/><style type=\"text/css\">body{font-family: monospace; font-size: 16px; padding: 25px 50px;}a{text-decoration: none; color: #19afdc;}</style></head><body><h1>Overview</h1>");
+        StringBuilder ovBuilder = new StringBuilder("<!Doctype html><html><head><title>Overview</title><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><meta name=\"robots\" content=\"none\"/><style type=\"text/css\">body{font-family: monospace; font-size: 16px; padding: 25px 50px;}a{text-decoration: none; color: #19afdc;}footer{position: fixed; left: 0; bottom: 0; background: #f2d9ff; width: 100%; padding: 5px; text-align: center;}</style></head><body><h1>Overview</h1><br><br>");
+
+        StringBuilder sideMenuBuilder = new StringBuilder();
+
+        for (ConfigSchema<?> schema : schemas){
+            String title = schema.getSchemaClass().getSimpleName();
+            sideMenuBuilder.append("<p><a href=\"").append(title).append(".html\">").append(title).append("</a></p>");
+        }
+
+        String sideMenuStr = sideMenuBuilder.toString();
 
         for (ConfigSchema<?> schema : schemas){
             String title = schema.getSchemaClass().getSimpleName();
             Collection<String> keys = schema.listKeys();
             ovBuilder.append("<h3>").append("<a href=\"").append(title).append(".html\">").append(title).append("</a>: ").append(keys.size()).append(" entries </h3>");
 
-            StringBuilder confBuilder = new StringBuilder("<!Doctype html><html><head><title>Config | ").append(title).append("</title><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><meta name=\"robots\" content=\"none\"/><style type=\"text/css\">body{font-family: monospace; font-size: 16px; padding: 25px 50px;}a{text-decoration: none; color: #19afdc;}table{width: 100%;}table th{font-size: 18px; background-color: #63c37c;}table, table td, table th{border: 1px solid #555; border-collapse: collapse; padding: 7px 12px;}</style></head><body><a href=\"index.html\">Overview</a> | <a href=\"javascript:window.history.back()\">Back</a><br/><br/><h1>").append(title).append("</h1><table><tr><th>Key</th><th>Type</th><th>Explanation</th></tr>");
+            StringBuilder confBuilder = new StringBuilder("<!Doctype html><html><head><title>Config | ").append(title).append("</title><meta charset=\"UTF-8\"/><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"/><meta name=\"robots\" content=\"none\"/><style type=\"text/css\">body{font-family: monospace; font-size: 16px;}a{text-decoration: none; color: #19afdc;}table{width: 100%;}table th{font-size: 18px; background-color: #63c37c;}table, table td, table th{border: 1px solid #555; border-collapse: collapse; padding: 7px 12px;}table tr:hover{background: #eee;}header{padding: 15px 50px;}#container{display: grid; grid-template-columns: auto auto auto auto auto auto;grid-gap: 20px;}#right-side{grid-column: 2 / 6;}#left-side{border-right: 1px #e6e6e6 solid;padding: 0 20px;font-size: 16px;text-transform: lowercase;}footer{position: fixed; left: 0; bottom: 0; background: #f2d9ff; width: 100%; padding: 5px; text-align: center;}</style></head><body><header><a href=\"index.html\">Overview</a> | <a href=\"javascript:window.history.back()\">Back</a><br/><br/><h1>").append(title).append("</h1></header><div id=\"container\"><div id=\"left-side\">").append(sideMenuStr).append("</iframe></div><div id=\"right-side\"><table><tr><th>Key</th><th>Type</th><th>Explanation</th></tr>");
 
             for (String key : keys){
                 ConfigSchema.Entry entry = schema.getEntry(key);
@@ -73,7 +90,7 @@ public class ConfigDoc {
                 }
                 confBuilder.append("</td></tr>");
             }
-            confBuilder.append("</table></body></html>");
+            confBuilder.append("</table></div></div>").append(footer).append("</body></html>");
 
             File file = new File(output, title+".html");
             try {
@@ -84,6 +101,7 @@ public class ConfigDoc {
             }
         }
 
+        ovBuilder.append(footer).append("</body></html>");
         File file = new File(output, "index.html");
         try {
             file.createNewFile();
